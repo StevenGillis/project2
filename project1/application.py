0739c15f5388263bc2b19eb695748a535f59a489
StevenGillis <s.gillis40@gmail.com>
2019-03-06 20:40:28 +0300

    second commit

--- project1/application.py
+++ project1/application.py
@@ -8,9 +8,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.debug = True
+
 # Check for environment variable
-#if not os.getenv("DATABASE_URL"):
-    #raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -19,8 +20,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-#engine = create_engine(os.getenv("DATABASE_URL"))
-#db = scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
